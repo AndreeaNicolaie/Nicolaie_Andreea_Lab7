4520ef085a613bdb530b28cee08a114a592f260a
ANDREEA NICOLAIE <andreea.nicolaie@stud.ubbcluj.ro>
2023-11-23 09:05:40 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Nicolaie_Andreea_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Nicolaie_Andreea_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
